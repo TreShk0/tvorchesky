f3b3b5c77220387d2090599c55528bf254b42d26
TreShk0 <tr3shk0@yandex.ru>
2023-04-07 22:01:18 +0700

    SMTP code written, needs to be refined, change mail server

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+#Импорт необходимых модулей
 import pandas as pd
+import smtplib
 
+#Создание датафрейма table
 table = pd.read_csv('dataset.csv', delimiter=',')
+#Группировка и создание результирующей таблицы, сохранение ее в файл
 result = table.groupby(['platform', 'event']).size()
 result.to_csv('result.csv', sep=',')
+
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj.starttls()
+
+smtpObj.login('login', 'password')
+
+smtpObj.sendmail("treshkobattle@gmai.com","tr3shk0@yandex.ru","test")
+
+smtpObj.quit()
