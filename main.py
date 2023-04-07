3e5fa4b9833e6025132a9f1a3756f33bbabac58f
TreShk0 <tr3shk0@yandex.ru>
2023-04-08 01:51:55 +0700

    Добавлена функция SendMessage, прикрепление файла к сообщению

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 
 #Создание датафрейма table
@@ -11,23 +12,29 @@ table = pd.read_csv('dataset.csv', delimiter=',')
 result = table.groupby(['platform', 'event']).size()
 result.to_csv('result.csv', sep=',')
 
+def SendMessage(to_adress):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = 'tvorcheskiRGF@yandex.ru'
+        msg['To'] = to_adress
+        msg['Subject'] = 'Отправка результатов творческого проекта'
+        message = 'К этому сообщению прикреплен csv файл - результат обработки датасета'
+        msg.attach(MIMEText(message))
+        filename='result.csv'
+        fp=open(filename,'rb')
+        att = MIMEApplication(fp.read(),_subtype="csv")
+        fp.close()
+        att.add_header('Content-Disposition','attachment',filename=filename)
+        msg.attach(att)
+        mail = smtplib.SMTP('smtp.yandex.ru',587)
+        mail.set_debuglevel(True)
+        mail.starttls()
+        mail.login('tvorcheskiRGF@yandex.ru', 'wwfdpcikojevhvuk')
+        mail.sendmail("tvorcheskiRGF@yandex.ru",to_adress,msg.as_string())
+        mail.quit()
+        print("Результаты отправлены")
+    except smtplib.SMTPException:
+        print("Не удалось отправить письмо, что-то пошло не так")
 
-
-msg = MIMEMultipart()
-msg['From'] = 'tvorcheskiRGF@yandex.ru'
-msg['To'] = 'tr3shk0@yandex.ru'
-msg['Subject'] = 'Тест скрипта SMTP'
-message = 'Тестовое сообщение для отправки через скрипт'
-msg.attach(MIMEText(message))
-
-
-mail = smtplib.SMTP('smtp.yandex.ru',587)
-mail.set_debuglevel(True)
-
-mail.starttls()
-
-mail.login('tvorcheskiRGF@yandex.ru', 'wwfdpcikojevhvuk')
-
-mail.sendmail("tvorcheskiRGF@yandex.ru","tr3shk0@yandex.ru",msg.as_string())
-
-mail.quit()
+to_adress = input("Введите адрес получателя: ")
+SendMessage(to_adress)
